aa93f0c8971e951ffcfe6eca1007c9657f066b1c
Civitaspo <civitaspo@gmail.com>
2017-03-03 19:01:44 +0900

    Remove Traceable module cuz doesnt work

--- lib/embulk/input/googlespreadsheet/error.rb
+++ lib/embulk/input/googlespreadsheet/error.rb
@@ -1,7 +1,8 @@
 module Embulk
   module Input
     class Googlespreadsheet < InputPlugin
-      module Traceable
+
+      class ConfigError < ::Embulk::ConfigError
         def initialize(e)
           message = "(#{e.class}) #{e}.\n\t#{e.backtrace.join("\t\n")}\n"
           while e.respond_to?(:cause) and e.cause
@@ -14,12 +15,17 @@ module Embulk
         end
       end
 
-      class ConfigError < ::Embulk::ConfigError
-        include Traceable
-      end
-
       class DataError < ::Embulk::DataError
-        include Traceable
+        def initialize(e)
+          message = "(#{e.class}) #{e}.\n\t#{e.backtrace.join("\t\n")}\n"
+          while e.respond_to?(:cause) and e.cause
+            # Java Exception cannot follow the JRuby causes.
+            message << "Caused by (#{e.cause.class}) #{e.cause}\n\t#{e.cause.backtrace.join("\t\n")}\n"
+            e = e.cause
+          end
+
+          super(message)
+        end
       end
 
       class CompatibilityError < DataError; end

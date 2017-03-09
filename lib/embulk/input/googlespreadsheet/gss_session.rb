98fdc28ea804ff56f948001194be0bb8ad9a9768
Civitaspo <civitaspo@gmail.com>
2017-03-09 18:07:03 +0900

    Fetch by 10000 rows

--- lib/embulk/input/googlespreadsheet/gss_session.rb
+++ lib/embulk/input/googlespreadsheet/gss_session.rb
@@ -1,5 +1,5 @@
-require "google_drive"
-require "signet/oauth_2/client"
+require "googleauth"
+require "google/apis/sheets_v4"
 require "json"
 require_relative 'error'
 
@@ -9,6 +9,10 @@ module Embulk
     class Googlespreadsheet < InputPlugin
 
       class GssSession
+        OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+        APPLICATION_NAME = 'embulk-input-googlespreadsheet'
+        SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY
+        FETCH_MAX = 10000 # rows
 
         def initialize(task)
           @task = task
@@ -16,7 +20,7 @@ module Embulk
           @auth_method = @task["auth_method"]
           @account = @task["account"]
           @json_keyfile = @task["json_keyfile"]
-          @client = nil
+          @credentials = nil
 
           @gss_key = @task["spreadsheet_key"]
           @ws_title = @task["worksheet_title"]
@@ -33,24 +37,22 @@ module Embulk
           end
 
           case @auth_method
-          when "json_key"
-            @client = Signet::OAuth2::Client.new(
-              :token_credential_uri => "https://accounts.google.com/o/oauth2/token",
-              :audience             => "https://accounts.google.com/o/oauth2/token",
-              :scope                => "https://www.googleapis.com/auth/drive" << " https://spreadsheets.google.com/feeds/",
-              :issuer               => @account,
-              :signing_key          => OpenSSL::PKey::RSA.new(key["private_key"])
+          when 'json_key'
+            @credentials = Google::Auth::ServiceAccountCredentials.new(
+              token_credential_uri: key['token_uri'],
+              audience:             key['token_uri'],
+              scope:                SCOPE,
+              issuer:               key['client_email'],
+              signing_key:          OpenSSL::PKey::RSA.new(key['private_key'])
             )
 
-          when "refresh_token"
-            @client = Signet::OAuth2::Client.new(
-              :token_credential_uri => "https://accounts.google.com/o/oauth2/token",
-              :audience             => "https://accounts.google.com/o/oauth2/token",
-              :scope                => "https://www.googleapis.com/auth/drive" << " https://spreadsheets.google.com/feeds/",
-              :redirect_uri         => "urn:ietf:wg:oauth:2.0:oob",
-              :client_id            => key["client_id"],
-              :client_secret        => key["client_secret"],
-              :refresh_token        => key["refresh_token"]
+          when 'refresh_token'
+            @credentials = Google::Auth::UserRefreshCredentials.new(
+              client_id:     key['client_id'],
+              client_secret: key['client_secret'],
+              refresh_token: key['refresh_token'],
+              scope:         SCOPE,
+              redirect_uri:  OOB_URI
             )
 
           else
@@ -58,41 +60,67 @@ module Embulk
           end
 
           begin
-            @client.fetch_access_token!
-            @session = GoogleDrive.login_with_oauth(@client.access_token)
+            @credentials.fetch_access_token!
+            @session = Google::Apis::SheetsV4::SheetsService.new.tap do |svc|
+              svc.client_options.application_name = APPLICATION_NAME
+              svc.authorization = @credentials
+            end
+
           rescue => e
             raise ConfigError.new(e)
           end
         end
 
-        def fetch(start_row, start_col, end_row, end_col)
+        def fetch(start_row, start_col, end_row, end_col, &block)
           if (end_row != -1 && start_row > end_row) || start_col > end_col
             raise ConfigError.new("Area not exist. Please check start&end for row and column.")
           end
 
           begin
-            ws = session.spreadsheet_by_key(@gss_key).worksheet_by_title(@ws_title)
-            raise ConfigError.new("target worksheet `#{@ws_title}` does not exist.") unless ws
-
-            row_num = 0
+            last_fatch_row_num = start_row - 1
             while true do
-              row_num += 1
-
-              values = (start_col..end_col).map do |col_num|
-                ws[row_num, col_num]
+              start_row_num = last_fatch_row_num + 1
+              end_row_num = last_fatch_row_num + FETCH_MAX
+              if end_row != -1 and end_row_num >= end_row
+                end_row_num = end_row
               end
-              break if end_row == -1 and values.all?{|v| v.nil? or v.empty?}
 
-              yield(values)
+              range = "#{@ws_title}!#{convert_col_name(start_col)}#{start_row_num}:#{convert_col_name(end_col)}#{end_row_num}"
 
-              break if end_row != -1 and row_num >= end_row
+              breakable = false
+              @session.get_spreadsheet_values(@gss_key, range).values.each do |row|
+                if end_row == -1 and row.all?{|v| v.nil? or v.empty?}
+                  breakable = true
+                  break
+                end
+                yield(row)
+              end
+              break if breakable
+
+              if end_row != -1 and end_row_num >= end_row
+                break
+              end
+
+              last_fatch_row_num += FETCH_MAX
             end
-
           rescue => e
             Embulk.logger.error { "embulk-input-googlespreadsheet: failed to fetch data from spreadsheet:#{@gss_key}, worksheet:#{@ws_title}, start_row:#{start_row}, end_row:#{end_row}, start_column:#{start_col}, end_column:#{end_col}, error:#{e}" }
             raise DataError.new(e)
           end
         end
+
+        COL_START_OFFSET = 'A'.ord
+        COL_NAME_BASE = 26
+        def convert_col_name(num)
+          [].tap do |r|
+            while num > 0
+              num -= 1
+              r.unshift (num % COL_NAME_BASE + COL_START_OFFSET).chr
+              num /= COL_NAME_BASE
+            end
+          end.join
+        end
+
 
       end
 

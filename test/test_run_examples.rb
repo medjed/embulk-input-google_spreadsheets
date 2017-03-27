006fab616d820c4b187ede25d017731b8cc15dd9
Civitaspo <civitaspo@gmail.com>
2017-03-27 10:51:49 +0900

    Add emoji config tests (now omitted due to https://github.com/embulk/embulk/issues/574)

--- test/test_run_examples.rb
+++ test/test_run_examples.rb
@@ -1,4 +1,5 @@
 require_relative 'helper'
+require 'embulk/input/google_spreadsheets'
 
 module Embulk
   class Input::GoogleSpreadsheets < InputPlugin
@@ -36,6 +37,26 @@ module Embulk
         else
           test 'service_account' do
             assert_true embulk_run(File.join(EXAMPLE_ROOT, 'config_service_account.yml'))
+          end
+        end
+      end
+
+      sub_test_case 'run emoji worksheet title' do
+        unless File.exist?(JSON_KEYFILE_AUTHORIZED_USER)
+          puts "'#{JSON_KEYFILE_AUTHORIZED_USER}' is not found. Skip case authorized_user"
+        else
+          test 'authorized_user' do
+            omit 'Skip until resolving https://github.com/embulk/embulk/issues/574'
+            assert_true embulk_run(File.join(EXAMPLE_ROOT, 'config_authorized_user_emoji_worksheet.yml'))
+          end
+        end
+
+        unless File.exist?(JSON_KEYFILE_SERVICE_ACCOUNT)
+          puts "'#{JSON_KEYFILE_SERVICE_ACCOUNT}' is not found. Skip case service_account"
+        else
+          test 'service_account' do
+            omit 'Skip until resolving https://github.com/embulk/embulk/issues/574'
+            assert_true embulk_run(File.join(EXAMPLE_ROOT, 'config_service_account_emoji_worksheet.yml'))
           end
         end
       end

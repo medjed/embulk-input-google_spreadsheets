6d73f00434693069f94609a0d18a27230551a11c
Civitaspo <civitaspo@gmail.com>
2017-04-04 18:36:35 +0900

    Update google-api-ruby-client >= v0.11.0

--- test/test_spreadsheets_client.rb
+++ test/test_spreadsheets_client.rb
@@ -75,8 +75,6 @@ else
         end
 
         test 'multi byte worksheet title' do
-          omit 'Skip until closing this issue https://github.com/medjed/embulk-input-google_spreadsheets/issues/6'
-
           assert_embulk_nothing_raised do
             client('worksheet_title' => TEST_WORKSHEET_TITLE_MULTI_BYTE).worksheet_values('A1:C1')
           end
